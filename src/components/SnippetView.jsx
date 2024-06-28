e5bcaf4782b487ce230346674cf1c6c7cd9304dc
LazyShuyaa <168578401+LazyShuyaa@users.noreply.github.com>
2024-06-28 12:24:29 +0530

    Update SnippetView.jsx

--- src/components/SnippetView.jsx
+++ src/components/SnippetView.jsx
@@ -85,26 +85,26 @@ const SnippetView = () => {
                     className="block px-4 py-2 text-sm text-white hover:bg-gray-900"
                     onClick={closeDropdown}
                   >
-                    Raw Code
+                    <FiPlus className="inline-block mr-2" /> Raw Code
                   </Link>
                   <button
                     onClick={() => { copyToClipboard(); closeDropdown(); }}
                     className="block w-full text-left px-4 py-2 text-sm text-white hover:bg-gray-900"
                   >
-                    Copy Code
+                    <FiCopy className="inline-block mr-2" /> Copy Code
                   </button>
                   <button
                     onClick={() => { shareUrl(); closeDropdown(); }}
                     className="block w-full text-left px-4 py-2 text-sm text-white hover:bg-gray-900"
                   >
-                    Share URL
+                    <FiShare2 className="inline-block mr-2" /> Share URL
                   </button>
                   <Link
                     to="/"
                     className="block w-full text-left px-4 py-2 text-sm text-white hover:bg-gray-900"
                     onClick={closeDropdown}
                   >
-                    Create New
+                    <FiPlus className="inline-block mr-2" /> Create New
                   </Link>
                 </div>
               </div>
